5030f328cc15f9c63648b06051463b0f9400232d
Ansgar Dietrichs <adietrichs@gmail.com>
2021-07-21 14:46:42 +0200

    core/vm: implement opAuth

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -41,9 +41,10 @@ type Config struct {
 // ScopeContext contains the things that are per-call, such as stack and memory,
 // but not transients like pc and gas
 type ScopeContext struct {
-	Memory   *Memory
-	Stack    *Stack
-	Contract *Contract
+	Memory     *Memory
+	Stack      *Stack
+	Contract   *Contract
+	Authorized *common.Address
 }
 
 // keccakState wraps sha3.state. In addition to the usual hash methods, it also supports
